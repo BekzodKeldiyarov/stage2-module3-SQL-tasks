93079f541acb65e2e26212cace15787e5801e9aa
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2023-02-19 00:59:40 +0500

    inline sql queries

--- lesson9/src/main/resources/SOLUTION.sql
+++ lesson9/src/main/resources/SOLUTION.sql
@@ -1,19 +1,5 @@
-select student.id, student.name, student.birthday, student.groupnumber
-from student
-         inner join mark on student.id = mark.student_id
-group by student_id
-having avg(mark) > 8;
+select student.id, student.name, student.birthday, student.groupnumber from student inner join mark on student.id = mark.student_id group by student_id having avg(mark) > 8;
 
+select student.id, student.name from student inner join mark on student.id = mark.student_id group by student.id having min(mark) > 7;
 
-select student.id, student.name
-from student
-         inner join mark on student.id = mark.student_id
-group by student.id
-having min(mark) > 7;
-
-select s.id, s.name
-from student s
-         inner join payment p on s.id = p.student_id
-where payment_date like '2019-%'
-group by p.student_id
-having count(student_id) > 2;
+select s.id, s.name from student s inner join payment p on s.id = p.student_id where payment_date like '2019-%' group by p.student_id having count(student_id) > 2;
