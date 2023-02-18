ae5259484eb58b590cebcb6a3ffca6b6c147d097
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2023-02-19 00:55:30 +0500

    5-fix

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,13 +1,18 @@
 select * from payment where amount >= 500;
 
-select * from student where DATEADD(NOW(), INTERVAL -20 YEAR) >= birthday;
+select * from student where DATEADD(YEAR, -20, CURRENT_DATE)>=birthday;
 
-select * from student where DATEADD(NOW(), INTERVAL -20 YEAR) <= birthday and groupnumber = 10;
+
+select * from student
+where DATEADD(YEAR, -20, CURRENT_DATE)<= birthday and groupnumber = 10;
 
 select * from student where name like '%Mike%' or groupnumber in (4, 5, 6);
 
-select * from payment where DATEADD(NOW(), INTERVAL -8 MONTH) <= PARSEDATETIME(payment_date, '%Y-%m-%d');
+
+select * from payment where FORMATDATETIME(DATEADD(MONTH, -8, CURRENT_DATE), 'yyyy-mm-dd') <= FORMATDATETIME(payment_date, 'yyyy-mm-dd');
 
 select * from student where name like 'A%';
 
-select * from student where ((name like 'Roxi%' and groupnumber = 4) or (name like 'Tallie%' and groupnumber = 9));
+select *
+from student
+where ((name like 'Roxi%' and groupnumber = 4) or (name like 'Tallie%' and groupnumber = 9));
