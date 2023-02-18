df5c1454c279fcffb4cedce43979c9a04696038b
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2023-02-19 00:18:20 +0500

    9 done

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,12 +1,12 @@
 select * from payment where amount >= 500;
 
-select * from student where DATE_ADD(NOW(), INTERVAL -20 YEAR) >= birthday;
+select * from student where DATEADD(NOW(), INTERVAL -20 YEAR) >= birthday;
 
-select * from student where DATE_ADD(NOW(), INTERVAL -20 YEAR) <= birthday and groupnumber = 10;
+select * from student where DATEADD(NOW(), INTERVAL -20 YEAR) <= birthday and groupnumber = 10;
 
 select * from student where name like '%Mike%' or groupnumber in (4, 5, 6);
 
-select * from payment where DATE_ADD(NOW(), INTERVAL -8 MONTH) <= DATE_FORMAT(payment_date, '%Y-%m-%d');
+select * from payment where DATEADD(NOW(), INTERVAL -8 MONTH) <= PARSEDATETIME(payment_date, '%Y-%m-%d');
 
 select * from student where name like 'A%';
 
--- lesson6/src/main/resources/SOLUTION.sql
+++ lesson6/src/main/resources/SOLUTION.sql
@@ -1,13 +1,23 @@
-select * from payment inner join paymenttype on payment.type_id = paymenttype.id
-where name='MONTHLY';
+select *
+from payment
+         inner join paymenttype p on payment.type_id = p.id
+where p.name = 'MONTHLY';
 
-select * from mark inner join subject on mark.subject_id = subject.id
-where subject.name='Art';
+select *
+from mark
+         inner join subject s on mark.subject_id = s.id
+where s.name = 'Art';
 
-select * from (student inner join payment on student.id=payment.student_id
-    inner join paymenttype on payment.type_id=paymenttype.id)
-where paymenttype.name='WEEKLY';
+select distinct(s.id), s.name, s.birthday, s.groupnumber
+from student s
+         inner join payment p on s.id = p.student_id
+         inner join paymenttype p2 on p.type_id = p2.id
+where p2.name = 'WEEKLY'
+order by s.id;
 
-select * from student inner join mark on student.id=mark.student_id
-                      inner join subject on mark.subject_id=subject.id
-where subject.name='Math';
+
+select *
+from student
+         inner join mark m on student.id = m.student_id
+         inner join subject s on m.subject_id = s.id
+where s.name = 'Math';
