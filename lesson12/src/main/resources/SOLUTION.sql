74786e8a5abd77b305a1cb6f6d7f33ac1ab453c5
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2023-02-19 04:37:07 +0500

    some fixes

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,3 +1,4 @@
+delete s from student s inner join mark m on s.id = m.student_id inner join subject s2 on m.subject_id = s2.id where s2.grade>=4;
 delete s from student s inner join mark m on s.id = m.student_id where m.mark<4;
 delete from paymenttype where name='DAILY';
 delete from mark where mark<7;
