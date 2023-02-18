75161f7dae8b7ee29eaa9defbf3768cc4c909354
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2023-02-19 04:06:02 +0500

    12-2-3-4 done

---
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -0,0 +1,3 @@
+delete s from student s inner join mark m on s.id = m.student_id where m.mark<4;
+delete from paymenttype where name='DAILY';
+delete from mark where mark<7;
--- lesson12/src/main/resources/TABLES_CHANGES.sql
+++ lesson12/src/main/resources/TABLES_CHANGES.sql
@@ -1,1 +1,9 @@
-select * from student;
+alter table mark drop foreign key mark_ibfk_1;
+alter table mark drop foreign key mark_ibfk_2;
+alter table mark add foreign key (student_id) references student(id) on DELETE CASCADE;
+alter table mark add foreign key (subject_id) references subject(id) on DELETE CASCADE;
+
+alter table payment drop foreign key payment_ibfk_1;
+alter table payment drop foreign key payment_ibfk_2;
+alter table payment add foreign key (student_id) references student(id) on DELETE CASCADE;
+alter table payment add foreign key (type_id) references paymenttype(id) on DELETE CASCADE;
